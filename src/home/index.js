d70db06f774e97d9529b6c2d932f3aba15645960
liuxinhui <liu.xinhu@northeastern.edu>
2022-12-05 20:36:26 -0500

    update app.js

---
+++ src/home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomeComponent = () => {
+  return (
+    <>
+      <h4>Home</h4>
+      {/* <WhatsHappening />
+      <TuitsList /> */}
+    </>
+  );
+};
+export default HomeComponent;
