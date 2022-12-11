f0f93fc00116af6f8fb8618f787427ea788b1650
liuxinhui <liu.xinhu@northeastern.edu>
2022-12-11 02:00:04 -0500

    fix

--- src/services/items/items-service.js
+++ src/services/items/items-service.js
@@ -2,17 +2,17 @@ import axios from 'axios';
 import { API_BASE } from "../api";
 const ITEMS_API = `${API_BASE}/items`;
 
-// const config = {
-//   headers: {
-//    'Access-Control-Allow-Origin' : '*',
-//    'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-//    }
-// }
+const config = {
+  headers: {
+   'Access-Control-Allow-Origin' : '*',
+   'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+   }
+}
 
-const headers ={
-  'Access-Control-Allow-Origin' : '*',
-  'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-  }
+// const headers ={
+//   'Access-Control-Allow-Origin' : '*',
+//   'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+  // }
 
 const api = axios.create({
    withCredentials: true
@@ -33,7 +33,7 @@ export const findRecentRemoteLikes = async (itemIds) => {
 }
 
 export const findRecentMongoLikes = async (itemIds) => {
-  const response = await api.post(`${ITEMS_API}/findRecentItems`, {headers: headers, itemIds: itemIds});
+  const response = await api.post(`${ITEMS_API}/findRecentItems`, { itemIds: itemIds},config);
   return response.data;
 }
 
