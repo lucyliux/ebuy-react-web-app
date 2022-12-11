8d11ea9c4a4bb948b7f836f4732654078bf88abb
liuxinhui <liu.xinhu@northeastern.edu>
2022-12-11 02:03:55 -0500

    fix

--- src/services/items/items-service.js
+++ src/services/items/items-service.js
@@ -5,7 +5,9 @@ const ITEMS_API = `${API_BASE}/items`;
 const config = {
   headers: {
    'Access-Control-Allow-Origin' : '*',
-   'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+  //  'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+  "Access-Control-Allow-Headers":
+  "Origin, X-Requested-With, Content-Type, Accept"
    }
 }
 
